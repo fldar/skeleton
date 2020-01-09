0b4bdde073d713bb3962576cf86525b7f5cec6fc
Francisco Lucas <lucasalmeidacco@gmail.com>
2020-01-09 11:23:51 -0300

    [RELEASE][SECURITY][1.1.0.0] Adjuste ExceptionHandleInterface

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,13 +20,4 @@ class SecurityController extends AbstractController
     {
         return $this->redirectToRoute('home');
     }
-
-    /**
-     * @Route("/re-login", name="/remember_not", methods={"GET"})
-     * @return Response
-     */
-    public function reLogin(): Response
-    {
-        return $this->redirectToRoute('home');
-    }
 }
